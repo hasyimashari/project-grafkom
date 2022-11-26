7fae039880b9bac72e7da780f14b69f5107dc034
misyah00 <hasyim11c@gmail.com>
2022-11-26 10:50:06 +0700

    add and change some files

---
+++ map/map.py
@@ -0,0 +1,32 @@
+import ctypes
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+w,h=800,600
+w_position,h_position = (ctypes.windll.user32.GetSystemMetrics(0)/2)-(w/2), (ctypes.windll.user32.GetSystemMetrics(1)/2)-(h/2)
+
+    
+def iterate():
+    glViewport(0, 0, 500, 500)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(-(w/2), w/2, -(h/2), h/2, 0.0, 1.0)
+    glMatrixMode (GL_MODELVIEW)
+    glLoadIdentity()
+
+def showScreen():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+    iterate()
+    glColor3f(1.0, 0.0, 3.0)
+    glutSwapBuffers()
+    
+glutInit()
+glutInitDisplayMode(GLUT_RGBA)
+glutInitWindowSize(w, h)
+glutInitWindowPosition(int(w_position), int(h_position))
+wind = glutCreateWindow("OpenGL Coding Practice")
+glutDisplayFunc(showScreen)
+glutIdleFunc(showScreen)
+glutMainLoop()
--- player/player.py
+++ player/player.py
@@ -1,8 +1,10 @@
+import ctypes
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 
 w,h=500,500
+w_position,h_position = (ctypes.windll.user32.GetSystemMetrics(0)/2)-(w/2), (ctypes.windll.user32.GetSystemMetrics(1)/2)-(h/2)
 
 def player():
     glColor3ub(0, 255, 0)
@@ -40,6 +42,7 @@ def iterate():
     glOrtho(-500, 500, -500, 500, 0.0, 1.0)
     glMatrixMode (GL_MODELVIEW)
     glLoadIdentity()
+
 def showScreen():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
@@ -47,10 +50,11 @@ def showScreen():
     glColor3f(1.0, 0.0, 3.0)
     player()
     glutSwapBuffers()
+    
 glutInit()
 glutInitDisplayMode(GLUT_RGBA)
 glutInitWindowSize(500, 500)
-glutInitWindowPosition(0, 0)
+glutInitWindowPosition(int(w_position), int(h_position))
 wind = glutCreateWindow("OpenGL Coding Practice")
 glutDisplayFunc(showScreen)
 glutIdleFunc(showScreen)

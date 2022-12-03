e0041d6d2ff4f208e27f1831d1b1a6ce8b2aada3
misyah00 <hasyim11c@gmail.com>
2022-12-03 15:22:58 +0700

    change map

--- map/map.py
+++ map/map.py
@@ -3,30 +3,32 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 
-w,h=800,600
+w,h = 800,600
 w_position,h_position = (ctypes.windll.user32.GetSystemMetrics(0)/2)-(w/2), (ctypes.windll.user32.GetSystemMetrics(1)/2)-(h/2)
 
-    
-def iterate():
-    glViewport(0, 0, w, h)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
+def linePos():
+    glColor3ub(255,255,255)
+    glBegin(GL_LINES)
+    glVertex2f(w, 0)
+    glVertex2f(-w, 0)
+    glVertex2f(0,h)
+    glVertex2f(0,-h)
+    glEnd()
+
+def init():
+    glClearColor(0.0, 0.0, 0.0, 1.0)
     glOrtho(-(w/2), w/2, -(h/2), h/2, 0.0, 1.0)
-    glMatrixMode (GL_MODELVIEW)
-    glLoadIdentity()
 
 def showScreen():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glLoadIdentity()
-    iterate()
-    glColor3f(1.0, 0.0, 3.0)
-    glutSwapBuffers()
-    
+    linePos()
+    glFlush()
+
 glutInit()
-glutInitDisplayMode(GLUT_RGBA)
+glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB)
 glutInitWindowSize(w, h)
 glutInitWindowPosition(int(w_position), int(h_position))
-wind = glutCreateWindow("OpenGL Coding Practice")
+glutCreateWindow("Project Pacman Wannabe")
 glutDisplayFunc(showScreen)
-glutIdleFunc(showScreen)
+init()
 glutMainLoop()

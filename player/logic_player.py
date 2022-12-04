f6740c43fd8fc7ef018aa24934cc035830b3acab
misyah00 <hasyim11c@gmail.com>
2022-12-05 02:14:01 +0700

    add center coor

--- player/logic_player.py
+++ player/logic_player.py
@@ -12,14 +12,15 @@ import playerIdle
 
 w,h = 800,600
 w_position,h_position = (ctypes.windll.user32.GetSystemMetrics(0)/2)-(w/2), (ctypes.windll.user32.GetSystemMetrics(1)/2)-(h/2)
-movex,movey = 0,0
+movex,movey = -40,-40
+centerx,centery = 0,0
 
 def init():
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
     glClearColor(0.0, 0.0, 0.0, 1.0)
     glOrtho(-(w/2), w/2, -(h/2), h/2, 0.0, 1.0)
-    print(movex, movey)
+    print(centerx, centery)
 
 def update(value):
     glutReshapeWindow(w,h)
@@ -27,9 +28,20 @@ def update(value):
     glutPostRedisplay()
     glutTimerFunc(25,update,0)
 
+def linepos():
+    glColor3ub(255, 255, 255)
+    glBegin(GL_LINES)
+    glVertex2f(-(w/2), 0)
+    glVertex2f(w/2, 0)
+    glVertex2f(0, -(h/2))
+    glVertex2f(0, h/2)
+    glEnd()
+
 def player_idle():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     init()
+    linepos()
+    glTranslated(movex,movey,0)
     playerIdle.player()
     playerIdle.lubang()
     playerIdle.matakiri()
@@ -39,12 +51,15 @@ def player_idle():
     glFlush()
 
 def player_right():
-    global movex, movey
+    global movex, movey, centerx, centery
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     init()
+    linepos()
     glTranslated(movex,movey,0)
     movex+=1.5
     movey+=0
+    centerx+=1.5
+    centery+=0
     playerRigh.player()
     playerRigh.lubang()
     playerRigh.matakiri()
@@ -54,12 +69,15 @@ def player_right():
     glFlush()
 
 def player_left():
-    global movex, movey
+    global movex, movey, centerx, centery
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     init()
+    linepos()
     glTranslated(movex,movey,0)
     movex-=1.5
     movey-=0
+    centerx-=1.5
+    centery-=0
     playerLeft.player()
     playerLeft.lubang()
     playerLeft.matakiri()
@@ -69,12 +87,15 @@ def player_left():
     glFlush()
 
 def player_up():
-    global movex, movey
+    global movex, movey, centerx, centery
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     init()
+    linepos()
     glTranslated(movex,movey,0)
     movex+=0
     movey+=1.5
+    centerx+=0
+    centery+=1.5
     playerUp.player()
     playerUp.lubang()
     playerUp.matakiri()
@@ -84,12 +105,15 @@ def player_up():
     glFlush()
 
 def player_down():
-    global movex,movey
+    global movex, movey, centerx, centery    
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     init()
+    linepos()
     glTranslated(movex,movey,0)
     movex-=0
     movey-=1.5
+    centerx-=0
+    centery-=1.5
     playerDown.player()
     playerDown.lubang()
     playerDown.matakiri()
@@ -116,5 +140,4 @@ glutCreateWindow("Project Pacman Wannabe")
 glutDisplayFunc(player_idle)
 glutSpecialFunc(player_move)
 glutTimerFunc(10,update,0)
-# init()
 glutMainLoop()

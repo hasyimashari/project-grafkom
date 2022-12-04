f1c37a77781e87848601311ee489ea6ff8a56a9c
misyah00 <hasyim11c@gmail.com>
2022-12-04 09:28:35 +0700

    update player

--- player/playerDown.py
+++ player/playerDown.py
@@ -19,9 +19,52 @@ def player():
     glVertex2f(800, 300)
     glVertex2f(700, 200)
     glVertex2f(500, 100)
-    
+    glEnd()
 
-    
+    glColor3ub(30, 176, 30)
+    glLineWidth(5)
+    glBegin(GL_LINE_LOOP)
+    glVertex2f(300, 100)
+    glVertex2f(100, 200)
+    glVertex2f(0, 300)
+    glVertex2f(0, 500)
+    glVertex2f(100, 600)
+    glVertex2f(300, 700)
+    glVertex2f(500, 700)
+    glVertex2f(700, 600)
+    glVertex2f(800, 500)
+    glVertex2f(800, 300)
+    glVertex2f(700, 200)
+    glVertex2f(500, 100)
+    glEnd()
+
+def lubang():
+    glColor3ub(30, 176, 30)
+    glBegin(GL_POLYGON)
+    glVertex2f(73, 323)
+    glVertex2f(66, 296)
+    glVertex2f(87, 280)
+    glVertex2f(110, 292)
+    glVertex2f(107, 321)
+    glVertex2f(91, 337)
+    glEnd()
+
+    glBegin(GL_POLYGON)
+    glVertex2f(656, 290)
+    glVertex2f(635, 269)
+    glVertex2f(631, 237)
+    glVertex2f(653, 227)
+    glVertex2f(674, 244)
+    glVertex2f(675, 271)
+    glEnd()
+
+    glBegin(GL_POLYGON)
+    glVertex2f(194, 611)
+    glVertex2f(173, 594)
+    glVertex2f(174, 570)
+    glVertex2f(195, 560)
+    glVertex2f(216, 580)
+    glVertex2f(216, 607)
     glEnd()
 
 def matakiri():
@@ -35,8 +78,8 @@ def matakiri():
     glVertex2f(350,400)
     glVertex2f(350,500)
     glVertex2f(300,550)
-    
     glEnd()
+
 def matakanan():
     glColor3ub(255,255,255)
     glBegin(GL_POLYGON)
@@ -48,7 +91,6 @@ def matakanan():
     glVertex2f(600,400)
     glVertex2f(600,500)
     glVertex2f(550,550)
-    
     glEnd()
     
 def pusatmatakanan():
@@ -83,6 +125,7 @@ def showScreen():
     iterate()
     glColor3f(1.0, 0.0, 3.0)
     player()
+    lubang()
     matakiri()
     matakanan()
     pusatmatakanan()

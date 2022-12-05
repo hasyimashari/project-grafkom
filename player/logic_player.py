3fef1014c3474ecd5da3ead1f42a57b69a6fb2dc
misyah00 <hasyim11c@gmail.com>
2022-12-06 04:31:34 +0700

    add logic player

--- player/logic_player.py
+++ player/logic_player.py
@@ -10,134 +10,81 @@ import playerRigh
 import playerUp
 import playerIdle
 
-w,h = 800,600
-w_position,h_position = (ctypes.windll.user32.GetSystemMetrics(0)/2)-(w/2), (ctypes.windll.user32.GetSystemMetrics(1)/2)-(h/2)
 movex,movey = -40,-40
 centerx,centery = 0,0
-
-def init():
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    glClearColor(0.0, 0.0, 0.0, 1.0)
-    glOrtho(-(w/2), w/2, -(h/2), h/2, 0.0, 1.0)
-    print(centerx, centery)
-
-def update(value):
-    glutReshapeWindow(w,h)
-    glutPositionWindow(int(w_position), int(h_position))
-    glutPostRedisplay()
-    glutTimerFunc(25,update,0)
-
-def linepos():
-    glColor3ub(255, 255, 255)
-    glBegin(GL_LINES)
-    glVertex2f(-(w/2), 0)
-    glVertex2f(w/2, 0)
-    glVertex2f(0, -(h/2))
-    glVertex2f(0, h/2)
-    glEnd()
+step_x, step_y = 0,0
 
 def player_idle():
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    init()
-    linepos()
-    glTranslated(movex,movey,0)
     playerIdle.player()
     playerIdle.lubang()
     playerIdle.matakiri()
     playerIdle.matakanan()
     playerIdle.pusatmatakanan()
     playerIdle.pusatmatakiri()
-    glFlush()
 
 def player_right():
-    global movex, movey, centerx, centery
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    init()
-    linepos()
-    glTranslated(movex,movey,0)
-    movex+=2
-    movey+=0
-    centerx+=2
-    centery+=0
     playerRigh.player()
     playerRigh.lubang()
     playerRigh.matakiri()
     playerRigh.matakanan()
     playerRigh.pusatmatakanan()
     playerRigh.pusatmatakiri()
-    glFlush()
 
 def player_left():
-    global movex, movey, centerx, centery
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    init()
-    linepos()
-    glTranslated(movex,movey,0)
-    movex-=2
-    movey-=0
-    centerx-=2
-    centery-=0
     playerLeft.player()
     playerLeft.lubang()
     playerLeft.matakiri()
     playerLeft.matakanan()
     playerLeft.pusatmatakanan()
     playerLeft.pusatmatakiri()
-    glFlush()
 
 def player_up():
-    global movex, movey, centerx, centery
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    init()
-    linepos()
-    glTranslated(movex,movey,0)
-    movex+=0
-    movey+=2
-    centerx+=0
-    centery+=2
     playerUp.player()
     playerUp.lubang()
     playerUp.matakiri()
     playerUp.matakanan()
     playerUp.pusatmatakanan()
     playerUp.pusatmatakiri()
-    glFlush()
 
 def player_down():
-    global movex, movey, centerx, centery    
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    init()
-    linepos()
-    glTranslated(movex,movey,0)
-    movex-=0
-    movey-=2
-    centerx-=0
-    centery-=2
     playerDown.player()
     playerDown.lubang()
     playerDown.matakiri()
     playerDown.matakanan()
     playerDown.pusatmatakanan()
     playerDown.pusatmatakiri()
-    glFlush()
 
-def player_move(key, x, y):
-    if key == GLUT_KEY_RIGHT:
-        glutDisplayFunc(player_right)
-    elif key == GLUT_KEY_LEFT:
-        glutDisplayFunc(player_left)
-    elif key == GLUT_KEY_UP:
-        glutDisplayFunc(player_up)
+def player_move():
+    global movex, movey, centerx, centery, step_x, step_y
+    glTranslated(movex,movey,0)
+    movex+=step_x
+    movey+=step_y
+    centerx+=step_x
+    centery+=step_y
+    if step_x==0 and step_y==0:
+        player_idle()
+    elif step_x==0 and step_y==2:
+        player_up()
+    elif step_x==0 and step_y==-2:
+        player_down()
+    elif step_x==2 and step_y==0:
+        player_right()
+    elif step_x==-2 and step_y==0:
+        player_left()
+    glTranslated(movex, movey, 0)
+
+def input_keyboard_player(key,x,y):
+    global step_x, step_y
+    if key == GLUT_KEY_UP:
+        step_x = 0
+        step_y = 2
     elif key == GLUT_KEY_DOWN:
-        glutDisplayFunc(player_down)
+        step_x = 0
+        step_y = -2
+    elif key == GLUT_KEY_RIGHT:
+        step_x = 2
+        step_y = 0
+    elif key == GLUT_KEY_LEFT:
+        step_x = -2
+        step_y = 0
 
-glutInit()
-glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB)
-glutInitWindowSize(w, h)
-glutInitWindowPosition(int(w_position), int(h_position))
-glutCreateWindow("Project Pacman Wannabe")
-glutDisplayFunc(player_idle)
-glutSpecialFunc(player_move)
-glutTimerFunc(10,update,0)
-glutMainLoop()

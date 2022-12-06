ecd4d08fc464b95e29a4422d1962bd1a37e60164
Unmates <farellishaktibaihaqi@gmail.com>
2022-12-07 00:56:14 +0700

    little change

--- stage/map/map1.py
+++ stage/map/map1.py
@@ -3,7 +3,7 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 
-w,h=1200,800
+w,h=1200,750
 w_position,h_position = (ctypes.windll.user32.GetSystemMetrics(0)/2)-(w/2), (ctypes.windll.user32.GetSystemMetrics(1)/2)-(h/2)
 
 def map1():
@@ -12,26 +12,26 @@ def map1():
 #kotak luar
     glColor3ub(240,25,10)
     glBegin(GL_POLYGON)
-    glVertex2f(-250, -350)
-    glVertex2f(-250, 250)
-    glVertex2f(-250, 250)
-    glVertex2f(250, 250)
-    glVertex2f(250, 250)
-    glVertex2f(250, -350)
-    glVertex2f(250, -350)
-    glVertex2f(-250, -350)
+    glVertex2f(-350, -400)
+    glVertex2f(-350, 300)
+    glVertex2f(-350, 300)
+    glVertex2f(350, 300)
+    glVertex2f(350, 300)
+    glVertex2f(350, -400)
+    glVertex2f(350, -400)
+    glVertex2f(-350, -400)
     glEnd()
 #kotak dalam
     glColor3ub(0,0,0)
     glBegin(GL_POLYGON)
-    glVertex2f(-200, -300)
-    glVertex2f(-200, 200)
-    glVertex2f(-200, 200)
-    glVertex2f(200, 200)
-    glVertex2f(200, 200)
-    glVertex2f(200, -300)
-    glVertex2f(200, -300)
-    glVertex2f(-200, -300)
+    glVertex2f(-300, -350)
+    glVertex2f(-300, 250)
+    glVertex2f(-300, 250)
+    glVertex2f(300, 250)
+    glVertex2f(300, 250)
+    glVertex2f(300, -350)
+    glVertex2f(300, -350)
+    glVertex2f(-300, -350)
     glEnd()
 
 # def iterate():

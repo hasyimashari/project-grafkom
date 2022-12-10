b8ea037b5c3be4c8a608d3a1ab136750ca278348
misyah00 <hasyim11c@gmail.com>
2022-12-10 11:14:21 +0700

    add tutorial and change some code

--- menu/game_over.py
+++ menu/game_over.py
@@ -1,66 +1,37 @@
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
-import main_temp
 
 class Menu:
-    def __init__(self, x, y):
-        self.x1 = x
-        self.y1 = y
+    def __init__(self, xcenter, ycenter):   #titik tengah dari kotak
+        self.x = xcenter
+        self.y = ycenter
 
     def draw_MenuSelect(self):
         glColor3ub(212,212,212)
         glBegin(GL_POLYGON)
-        glVertex2f(self.x1-150, self.y1)
-        glVertex2f(self.x1-150, self.y1-50)
-        glVertex2f(self.x1-165, self.y1-66)
-        glVertex2f(self.x1-650, self.y1-66)
-        glVertex2f(self.x1-650, self.y1-10)
-        glVertex2f(self.x1-640, self.y1)
-        glEnd()
-
-    def draw_MenuNotSelect(self):
-        glColor3ub(0,0,0)
-        glBegin(GL_POLYGON)
-        glVertex2f(self.x1-200, self.y1-10)
-        glVertex2f(self.x1-200, self.y1-40)
-        glVertex2f(self.x1-210, self.y1-50)
-        glVertex2f(self.x1-600, self.y1-50)
-        glVertex2f(self.x1-600, self.y1-20)
-        glVertex2f(self.x1-590, self.y1-10)
-        glEnd()
-
-        glColor3ub(212,212,212)
-        glBegin(GL_LINES)
-        glVertex2f(self.x1-200, self.y1-10)
-        glVertex2f(self.x1-200, self.y1-40)
-        glVertex2f(self.x1-200, self.y1-40)
-        glVertex2f(self.x1-210, self.y1-50)
-        glVertex2f(self.x1-210, self.y1-50)
-        glVertex2f(self.x1-600, self.y1-50)
-        glVertex2f(self.x1-600, self.y1-50)
-        glVertex2f(self.x1-600, self.y1-20)
-        glVertex2f(self.x1-600, self.y1-20)
-        glVertex2f(self.x1-590, self.y1-10)
-        glVertex2f(self.x1-590, self.y1-10)
-        glVertex2f(self.x1-200, self.y1-10)
+        glVertex2f(self.x+220,self.y+55)
+        glVertex2f(self.x+220,self.y+1)          
+        glVertex2f(self.x+198,self.y-25)
+        glVertex2f(self.x-220,self.y-25)
+        glVertex2f(self.x-220,self.y+28)
+        glVertex2f(self.x-198,self.y+55)
         glEnd()
 
     def draw_text(self,text,r,b,g):
         color = (r, b, g)
         font_style = GLUT_BITMAP_TIMES_ROMAN_24
         glColor3ub(color[0],color[1],color[2])
-        line=0
-        glRasterPos2f(self.x1-525, self.y1-33)
+        glRasterPos2f(self.x-135, self.y+10)
         for i in text:
             glutBitmapCharacter(font_style, ord(i))
 
-def menu_back_selected():
-    menu = Menu(400, 34)
+def menu_back():
+    menu = Menu(0, 0)
     menu.draw_MenuSelect()
     menu.draw_text("press ESC to back to menu", 0, 0, 0)
 
 def back():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    menu_back_selected()
+    menu_back()
     glFlush()
